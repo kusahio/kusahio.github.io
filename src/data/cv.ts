1b4bd0daf5e3bd0aff8bbec9688538d13a30e557
CamiloIllanes <67984533+kusahio@users.noreply.github.com>
2026-02-01 16:46:40 -0300

    añadida sección de experiencia

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -39,7 +39,10 @@ interface CVData {
     github: string;
     phone: string;
   };
-  experience: Experience[];
+  experience: {
+    title: string,
+    jobs: Experience[];
+  };
   skills: Skill[];
 }
 
@@ -65,57 +68,60 @@ export const CV: CVData = {
     github: 'https://github.com/kusahio',
     phone: '+56990717236',
   },
-  experience: [
-    {
-      company: 'Red MTS',
-      role: 'Desarrollador Front-End / UI',
-      period: 'Jul 2020 - Oct 2024',
-      responsibilities: [
-        'Desarrollo de interfaces con React y componentes reutilizables.',
-        'Integración de aplicaciones con APIs REST.',
-        'Participación en migraciones de sistemas y optimización de flujos bajo arquitectura MVC.',
-        'Trabajo en equipos ágiles utilizando Git y control de versiones.'
-      ],
-      projects: [
-        {
-          title: 'Plataforma E-commerce MTS (Portal del socio)',
-          achievements: [
-            'Desarrollo de componentes frontend con React.',
-            'Integración con APIs REST.',
-            'Optimización de rendimiento y mantenibilidad.'
-          ]
-        },
-      ]
-    },
-    {
-      company: 'Walmart Chile',
-      role: 'Diseñador Web',
-      period: 'Jul 2017 - Ene 2020',
-      responsibilities: [
-        'Desarrollo y mantenimiento de interfaces web accesibles y responsive.',
-        'Mejora de semántica HTML y flujos mobile-first.',
-        'Participación en el diseño e implementación del checkout “Buy Smart”'
-      ],
-      projects: [
-        {
-          title: 'Checkout “Buy Smart” - Walmart',
-          achievements: [
-            'Implementación de interfaz accesible y optimizada para el proceso de pago.',
-            'Enfoque mobile-first y mejora de experiencia de usuario.'
-          ]
-        },
-      ]
-    },
-    {
-      company: 'Falabella Retail S.A',
-      role: 'Diseñador Web',
-      period: 'Feb 2017 - Jul 2017',
-      responsibilities: [
-        'Implementación de vistas web responsivas.',
-        'Soporte frontend utilizando JavaScript.',
-      ]
-    }
-  ],
+  experience: {
+    title: 'Experiencia',
+    jobs: [
+      {
+        company: 'Red MTS',
+        role: 'Desarrollador Front-End / UI',
+        period: 'Jul 2020 - Oct 2024',
+        responsibilities: [
+          'Desarrollo de interfaces con React y componentes reutilizables.',
+          'Integración de aplicaciones con APIs REST.',
+          'Participación en migraciones de sistemas y optimización de flujos bajo arquitectura MVC.',
+          'Trabajo en equipos ágiles utilizando Git y control de versiones.'
+        ],
+        projects: [
+          {
+            title: 'Plataforma E-commerce MTS (Portal del socio)',
+            achievements: [
+              'Desarrollo de componentes frontend con React.',
+              'Integración con APIs REST.',
+              'Optimización de rendimiento y mantenibilidad.'
+            ]
+          },
+        ]
+      },
+      {
+        company: 'Walmart Chile',
+        role: 'Diseñador Web',
+        period: 'Jul 2017 - Ene 2020',
+        responsibilities: [
+          'Desarrollo y mantenimiento de interfaces web accesibles y responsive.',
+          'Mejora de semántica HTML y flujos mobile-first.',
+          'Participación en el diseño e implementación del checkout “Buy Smart”'
+        ],
+        projects: [
+          {
+            title: 'Checkout “Buy Smart” - Walmart',
+            achievements: [
+              'Implementación de interfaz accesible y optimizada para el proceso de pago.',
+              'Enfoque mobile-first y mejora de experiencia de usuario.'
+            ]
+          },
+        ]
+      },
+      {
+        company: 'Falabella Retail S.A',
+        role: 'Diseñador Web',
+        period: 'Feb 2017 - Jul 2017',
+        responsibilities: [
+          'Implementación de vistas web responsivas.',
+          'Soporte frontend utilizando JavaScript.',
+        ]
+      }
+    ]
+  },
   skills: [
     { name: "React", icon: "react" },
     { name: "Next.js", icon: "nextjs" },
